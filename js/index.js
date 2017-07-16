88b39d36202f76b16005ad8ec470027b692dbee2
Oksana Buday <oksanabuday@gmail.com>
2017-07-16 13:23:09 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,46 +1,48 @@
-var app = angular.module("myapp", []);
-	app.controller("ListController", ['$scope', function($scope) {
-	     $scope.personalDetails = [
+
+ 
+ var app = angular.module("myapp", []);
+app.controller("ListController", ['$scope', function($scope) {
+    $scope.personalDetails = [
         {
-            'number':'2423543',
-            'doc':'Cthnbasrfn',
-            'reg':'Львів'
-            'reg':'Стрий'
-            'reg':'місто'
-        }
-       ];
-	    
-	        $scope.addNew = function(personalDetails){
-	            $scope.personalDetails.push({ 
-	                'number': personalDetails.number, 
-	                'doc': personalDetails.doc,
-	                'reg': personalDetails.reg, 
-	                'reg': personalDetails.reg, 
-	                'reg': personalDetails.reg,
-	            });
-	            $scope.PD = {};
-	        };
-	    
-	        $scope.remove = function(){
-	            var newDataList=[];
-	            $scope.selectedAll = false;
-	            angular.forEach($scope.personalDetails, function(selected){
-	                if(!selected.selected){
-	                    newDataList.push(selected);
-	                }
-	            }); 
-	            $scope.personalDetails = newDataList;
-	        };
-	    
-	        // $scope.checkAll = function () {
-	        //     if (!$scope.selectedAll) {
-	        //         $scope.selectedAll = true;
-	        //     } else {
-	        //         $scope.selectedAll = false;
-	        //     }
-	        //     angular.forEach($scope.personalDetails, function (personalDetails) {
-	        //         personalDetails.selected = $scope.selectedAll;
-	        //     });
-	        // }; 
-	    
-		}]);
+            'number':'1234756557:12:32:3432',
+            'reg':'Вінницька обл',
+            'district':'Стрийський р-н', 
+            'settlement':'Станків',
+            'doc':'Свідоцтво про право власності № 123'
+        },
+      ];
+    
+        $scope.addNew = function(personalDetails){
+            $scope.personalDetails.push({ 
+                'number': personalDetails.number, 
+                'reg': personalDetails.reg,
+                'district': personalDetails.district,
+                'settlement': personalDetails.settlement,
+                'doc': personalDetails.doc,
+            });
+            $scope.PD = {};
+        };
+    
+        // $scope.remove = function(){
+        //     var newDataList=[];
+        //     $scope.selectedAll = false;
+        //     angular.forEach($scope.personalDetails, function(selected){
+        //         if(!selected.selected){
+        //             newDataList.push(selected);
+        //         }
+        //     }); 
+        //     $scope.personalDetails = newDataList;
+        // };
+    
+        // $scope.checkAll = function () {
+        //     if (!$scope.selectedAll) {
+        //         $scope.selectedAll = true;
+        //     } else {
+        //         $scope.selectedAll = false;
+        //     }
+        //     angular.forEach($scope.personalDetails, function (personalDetails) {
+        //         personalDetails.selected = $scope.selectedAll;
+        //     });
+        // }; 
+    
+}]);
