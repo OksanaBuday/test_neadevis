089815e06f61161b9a8398d8a5785fcfccf98337
Oksana Buday <oksanabuday@gmail.com>
2017-07-16 20:16:29 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,65 +1,38 @@
-
- 
- var app = angular.module("myapp", []);
+var app = angular.module("myapp", []);
 app.controller("ListController", ['$scope', function($scope) {
-    $scope.personalDetails = [
-        {
-            'number':'1234756557:12:32:3432',
-            'reg':'Вінницька обл',
-            'district':'Стрийський р-н', 
-            'settlement':'Станків',
-            'doc':'Свідоцтво про право власності № 1254'
-        },
-      ];
-    
-        $scope.addNew = function(personalDetails){
-            $scope.personalDetails.push({ 
-                'number': personalDetails.number, 
-                'reg': personalDetails.reg,
-                'district': personalDetails.district,
-                'settlement': personalDetails.settlement,
-                'doc': personalDetails.doc,
-            });
-            $scope.PD = {};
-        };
-    
-        // $scope.remove = function(){
-        //     var newDataList=[];
-        //     $scope.selectedAll = false;
-        //     angular.forEach($scope.personalDetails, function(selected){
-        //         if(!selected.selected){
-        //             newDataList.push(selected);
-        //         }
-        //     }); 
-        //     $scope.personalDetails = newDataList;
-        // };
-    
-        // $scope.checkAll = function () {
-        //     if (!$scope.selectedAll) {
-        //         $scope.selectedAll = true;
-        //     } else {
-        //         $scope.selectedAll = false;
-        //     }
-        //     angular.forEach($scope.personalDetails, function (personalDetails) {
-        //         personalDetails.selected = $scope.selectedAll;
-        //     });
-        // }; 
-    
+	$scope.personalDetails = [{
+		'number': '1234756557:12:32:3432',
+		'reg': 'Вінницька обл',
+		'district': 'Стрийський р-н',
+		'settlement': 'Станків',
+		'doc': 'Свідоцтво про право власності № 1254'
+	}, ];
+
+	$scope.addNew = function(personalDetails) {
+		$scope.personalDetails.push({
+			'number': personalDetails.number,
+			'reg': personalDetails.reg,
+			'district': personalDetails.district,
+			'settlement': personalDetails.settlement,
+			'doc': personalDetails.doc,
+		});
+		$scope.PD = {};
+	};
 }]);
 
 function shorten(text, maxLength, delimiter, overflow) {
-  delimiter = delimiter || "&hellip;";
-  overflow = overflow || false;
-  var ret = text;
-  if (ret.length > maxLength) {
-    var breakpoint = overflow ? maxLength + ret.substr(maxLength).indexOf(" ") : ret.substr(0, maxLength).lastIndexOf(" ");
-    ret = ret.substr(0, breakpoint) + delimiter;
-  }
-  return ret;
+	delimiter = delimiter || "&hellip;";
+	overflow = overflow || false;
+	var ret = text;
+	if (ret.length > maxLength) {
+		var breakpoint = overflow ? maxLength + ret.substr(maxLength).indexOf(" ") : ret.substr(0, maxLength).lastIndexOf(" ");
+		ret = ret.substr(0, breakpoint) + delimiter;
+	}
+	return ret;
 }
 
 $(document).ready(function() {
-  var $editedText = $(".edited_text");
-  var text = $editedText.text();
-  $editedText.text(shorten(text, 40, "...", false));
+	var $editedText = $(".edited_text");
+	var text = $editedText.text();
+	$editedText.text(shorten(text, 40, "...", false));
 });
